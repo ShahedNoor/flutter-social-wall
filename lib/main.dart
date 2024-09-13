fab3c829b32e5956112f58e17955c49e9aea1e5e
Shahed Noor <shahednoor28@gmail.com>
2024-09-13 11:09:34 +0600

    SpinKit loader added

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,10 @@ import 'package:social_wall/pages/profile_page.dart';
 import 'package:social_wall/pages/user_page.dart';
 import 'package:social_wall/theme/dark_mode.dart';
 import 'package:social_wall/theme/light_mode.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -23,19 +23,49 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // Future to hold Firebase initialization
+  Future<FirebaseApp> _initializeFirebase() async {
+    return Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Social Wall",
-      debugShowCheckedModeBanner: false,
-      home: const AuthPage(),
-      theme: lightMode,
-      darkTheme: darkMode,
-      routes: {
-        "/loginOrRegister": (context) => const LoginOrRegister(),
-        "/homePage": (context) => HomePage(),
-        "/profilePage": (context) => ProfilePage(),
-        "/userPage": (context) => const UserPage(),
+    return FutureBuilder(
+      // Wait for Firebase to initialize
+      future: _initializeFirebase(),
+      builder: (context, snapshot) {
+        // If Firebase is still initializing, show loading spinner
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            title: "Social Wall",
+            debugShowCheckedModeBanner: false,
+            theme: lightMode,
+            darkTheme: darkMode,
+            home: Scaffold(
+              body: Center(
+                child: SpinKitWave(
+                  color: Theme.of(context).colorScheme.secondary,
+                  size: 50.0,
+                ),
+              ),
+            ),
+          );
+        }
+        // Once Firebase is initialized, show the app
+        return MaterialApp(
+          title: "Social Wall",
+          debugShowCheckedModeBanner: false,
+          home: const AuthPage(),
+          theme: lightMode,
+          darkTheme: darkMode,
+          routes: {
+            "/loginOrRegister": (context) => const LoginOrRegister(),
+            "/homePage": (context) => HomePage(),
+            "/profilePage": (context) => ProfilePage(),
+            "/userPage": (context) => const UserPage(),
+          },
+        );
       },
     );
   }
